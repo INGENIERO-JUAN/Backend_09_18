fe6335ae1587d38d757f01d5b2e2c9430ceb133b
INGENIERO-JUAN <nosequeponersolopongoesto@gmail.com>
2024-09-18 22:46:03 -0500

    commit-clase-backend-TS

---
+++ build/types/exampleTypes.js
@@ -0,0 +1,9 @@
+let numero = 42;
+let texto = "Hola, TypeScript!";
+let esVerdadero = true;
+let variableCualquiera = { nombre: "Juan", edad: 25 };
+console.log("Valor de número:", numero);
+console.log("Valor de texto:", texto);
+console.log("Valor de booleano:", esVerdadero);
+console.log("Valor de variable 'any':", variableCualquiera);
+//# sourceMappingURL=exampleTypes.js.map
---
+++ src/types/exampleTypes.ts
@@ -0,0 +1,10 @@
+let numero: number = 42;
+let texto: string = "Hola, TypeScript!";
+let esVerdadero: boolean = true;
+let variableCualquiera: any = { nombre: "Juan", edad: 25 };
+
+// Imprimiendo cada variable
+console.log("Valor de número:", numero);
+console.log("Valor de texto:", texto);
+console.log("Valor de booleano:", esVerdadero);
+console.log("Valor de variable 'any':", variableCualquiera);
