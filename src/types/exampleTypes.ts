31880e1d98c905a10de70a3f6139855f8e390bc5
INGENIERO-JUAN <nosequeponersolopongoesto@gmail.com>
2024-09-23 01:26:41 -0500

    commit-pruebas-y-correccion-Eslint

--- src/types/exampleTypes.ts
+++ src/types/exampleTypes.ts
@@ -1,10 +1,25 @@
-let numero: number = 42;
-let texto: string = "Hola, TypeScript!";
-let esVerdadero: boolean = true;
-let variableCualquiera: any = { nombre: "Juan", edad: 25 };
-
 // Imprimiendo cada variable
-console.log("Valor de número:", numero);
-console.log("Valor de texto:", texto);
-console.log("Valor de booleano:", esVerdadero);
-console.log("Valor de variable 'any':", variableCualquiera);
+const numero = 42;
+const texto = 'Hola, TypeScript!';
+const esVerdadero = true;
+const variableCualquiera = { nombre: 'Juan', edad: 25 };
+
+console.log('Valor de número:', numero);
+console.log('Valor de texto:', texto);
+console.log('Valor de booleano:', esVerdadero);
+console.log('Valor de variable "any":', variableCualquiera);
+
+// Otro ejemplo
+const edad = 30;
+const nombrePersona = 'Ana';
+const esEstudiante = true;
+const direccion = { calle: 'Calle 10', ciudad: 'Medellín', país: 'Colombia' };
+const amigos = ['Laura', 'Pedro', 'Juan', 'Sofía'];
+const altura = 1.65;
+
+console.log('Edad:', edad);
+console.log('Nombre:', nombrePersona);
+console.log('Es estudiante:', esEstudiante);
+console.log('Dirección:', direccion);
+console.log('Amigos:', amigos);
+console.log('Altura:', altura);
